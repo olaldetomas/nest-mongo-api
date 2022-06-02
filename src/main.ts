38258a3740e1ac31cc4edba9ebb68178d82af50a
Tomas Olalde <olaldetomas@gmail.com>
2022-06-02 16:02:13 -0300

    Add build-in logger

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from './config/config.service'
@@ -9,7 +10,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService)
 
   await app.listen(configService.port, () => {
-    console.log(`App listen on port: ${configService.port}`)
+    Logger.log(`App listen on port: ${configService.port}`)
   })
 }
 bootstrap()
